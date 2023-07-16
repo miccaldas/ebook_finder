f5bdaec7c1944c56bfc2d79d805f885a8551e8d9
micaldas <micaldas@mailfence.com>
2023-07-16 16:40:59 +0100

    16-07-2023 commit

--- ebook_finder/scraper/pdfdrive/pdfdrive/spiders/pdf.py
+++ ebook_finder/scraper/pdfdrive/pdfdrive/spiders/pdf.py
@@ -5,12 +5,12 @@ from snoop import pp
 
 
 class PdfSpider(scrapy.Spider):
-    name = 'pdf'
-    allowed_domains = ['www.pdfdrive.com']
-    start_urls = ['https://www.pdfdrive.com/search?q=tom+hardy', 'https://pdfdrive.com/search?q=tom hardy&page=2']
+    name = "pdf"
+    allowed_domains = ["www.pdfdrive.com"]
+    start_urls = ["https://www.pdfdrive.com/search?q=tom+hardy", "https://pdfdrive.com/search?q=tom hardy&page=2"]
 
     @snoop
     def parse(self, response):
         pd = PdfdriveItem()
-        pd['link'] = response.xpath('//div[@class="file-right"]/a/@href').getall()
+        pd["link"] = response.xpath('//div[@class="file-right"]/a/@href').getall()
         return pd
